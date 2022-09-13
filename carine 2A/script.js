7c3e783c4813fd840aea60f3b958e79b377f4bf2
carine amiratti <101649519+carineamirattii@users.noreply.github.com>
2022-09-13 12:55:02 -0300

    Add files via upload

---
+++ carine 2A/script.js
@@ -0,0 +1,16 @@
+const calcular = function() { 
+    let deslocamento = document.getElementById('deslocamento')
+    let tempo = document.getElementById('tempo')
+    let vm = document.getElementById('velocidadeMedia')
+    console.log(deslocamento.value, tempo.value);
+
+    vm.value = deslocamento.value / tempo.value
+    alert('Cálculo realizado com sucesso!')
+}
+
+const limpar = function(){
+    document.getElementById('deslocamento').value = ""
+    document.getElementById('tempo').value = ""
+    document.getElementById('velocidadeMedia').value = ""
+}
+
